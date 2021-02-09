be8a4e251697344dcf85b3c98c43d61537016159
ihor_debelyi <s15293@pjwstk.edu.pl>
2021-02-09 20:23:10 +0100

    created app story_path

---
+++ destini-challenge-starting/lib/story.dart
@@ -0,0 +1,13 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+  int storyNumChoice1;
+  int storyNumChoice2;
+  Story(
+      {this.storyTitle,
+      this.choice1,
+      this.choice2,
+      this.storyNumChoice1,
+      this.storyNumChoice2});
+}
